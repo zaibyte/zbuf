a627fde47fe9a4990e448485000932e8e58e24d6
templexxx <lucas1x1x@gmail.com>
2020-08-14 20:32:10 +0800

    extent/v1: put, get test & get perf test

--- extent/v1/extent_test.go
+++ extent/v1/extent_test.go
@@ -17,7 +17,9 @@
 package v1
 
 import (
+	"bytes"
 	"context"
+	"fmt"
 	"io/ioutil"
 	"math/rand"
 	"os"
@@ -35,7 +37,78 @@ import (
 	"github.com/zaibyte/zbuf/xio"
 )
 
-func TestExtentWritePerf(t *testing.T) {
+func TestExtentPutGet(t *testing.T) {
+	dataRoot, err := ioutil.TempDir(os.TempDir(), "extent_write")
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Println(dataRoot)
+	//defer os.RemoveAll(dataRoot)
+
+	cfg := &ExtentConfig{
+		DataRoot:    dataRoot,
+		SegmentCnt:  defaultSegmentCnt,
+		SegmentSize: 1024 * 1024,
+		FlushDelay:  time.Microsecond * 128,
+		PutPending:  2048,
+		InsertOnly:  false,
+	}
+
+	x := startXIOer()
+	defer x.close()
+
+	ext, err := New(cfg, 1, x.flushJobChan, x.getJobChan)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer ext.Close()
+
+	for i := 0; i < 1024; i++ {
+		obj := xbytes.GetNBytes(i)
+		b := obj.Bytes()[:i]
+		for j := range b {
+			b[j] = uint8(i)
+		}
+		obj.Set(b)
+
+		digest := xdigest.Sum32(obj.Bytes()[:i])
+
+		_, oids := uid.MakeOID(1, 1, digest, uint32(i), uid.NormalObj)
+		var oid [16]byte
+		xhex.Decode(oid[:], xstrconv.ToBytes(oids))
+		err = ext.PutObj(uint64(i), oid, obj)
+		if err != nil {
+			obj.Close()
+			t.Fatal(err)
+		}
+		obj.Close()
+	}
+
+	for i := 0; i < 1024; i++ {
+		b := make([]byte, i)
+		for j := range b {
+			b[j] = uint8(i)
+		}
+
+		digest := xdigest.Sum32(b)
+
+		_, oids := uid.MakeOID(1, 1, digest, uint32(i), uid.NormalObj)
+		var oid [16]byte
+		xhex.Decode(oid[:], xstrconv.ToBytes(oids))
+
+		getObj, err := ext.GetObj(uint64(i), oid)
+		if err != nil {
+			t.Fatal(err, i)
+		}
+		if !bytes.Equal(b, getObj.Bytes()) {
+			getObj.Close()
+			t.Fatal("get mismatch")
+		}
+		getObj.Close()
+	}
+}
+
+func TestExtentPutPerf(t *testing.T) {
 
 	dataRoot, err := ioutil.TempDir(os.TempDir(), "extent_write")
 	if err != nil {
@@ -47,8 +120,8 @@ func TestExtentWritePerf(t *testing.T) {
 		DataRoot:    dataRoot,
 		SegmentCnt:  defaultSegmentCnt,
 		SegmentSize: 1024 * 1024,
-		FlushDelay:  time.Microsecond * 100,
-		PutPending:  2048,
+		FlushDelay:  time.Microsecond * 128,
+		PutPending:  256,
 		InsertOnly:  false,
 	}
 
@@ -90,7 +163,82 @@ func TestExtentWritePerf(t *testing.T) {
 	wg.Wait()
 	end := tsc.UnixNano()
 	ops := float64(end-start) / float64(32768)
-	t.Logf("extent write perf: %.2f ns/op", ops)
+	t.Logf("extent put perf: %.2f ns/op", ops)
+}
+
+func TestExtentGetPerf(t *testing.T) {
+
+	dataRoot, err := ioutil.TempDir(os.TempDir(), "extent_write")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(dataRoot)
+
+	cfg := &ExtentConfig{
+		DataRoot:    dataRoot,
+		SegmentCnt:  defaultSegmentCnt,
+		SegmentSize: 1024 * 1024,
+		FlushDelay:  time.Microsecond * 128,
+		PutPending:  256,
+		InsertOnly:  false,
+	}
+
+	x := startXIOer()
+	defer x.close()
+
+	ext, err := New(cfg, 1, x.flushJobChan, x.getJobChan)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer ext.Close()
+
+	obj := xbytes.GetNBytes(3952)
+	defer obj.Close()
+
+	rand.Read(obj.Bytes()[:3952])
+	obj.Set(obj.Bytes()[:3952])
+
+	digest := xdigest.Sum32(obj.Bytes()[:3952])
+
+	_, oids := uid.MakeOID(1, 1, digest, 3952, uid.NormalObj)
+	var oid [16]byte
+	xhex.Decode(oid[:], xstrconv.ToBytes(oids))
+
+	// Fast write.
+	wg := new(sync.WaitGroup)
+	for i := 0; i < 128; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for j := 0; j < 256; j++ {
+				err := ext.PutObj(1, oid, obj)
+				if err != nil {
+					t.Fatal(err)
+				}
+			}
+		}()
+	}
+	wg.Wait()
+
+	wg2 := new(sync.WaitGroup)
+	start := tsc.UnixNano()
+	for i := 0; i < 512; i++ {
+		wg2.Add(1)
+		go func() {
+			defer wg2.Done()
+			for j := 0; j < 64; j++ {
+				objData, err := ext.GetObj(1, oid)
+				if err != nil {
+					t.Fatal(err)
+				}
+				objData.Close()
+			}
+		}()
+	}
+	wg2.Wait()
+	end := tsc.UnixNano()
+	ops := float64(end-start) / float64(32768)
+	t.Logf("extent get perf: %.2f ns/op", ops)
 }
 
 type xioer struct {
