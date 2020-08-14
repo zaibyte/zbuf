2626059c2d2bddeba3d44950be82db9c7bc9c2d1
templexxx <lucas1x1x@gmail.com>
2020-08-15 00:01:51 +0800

    server: create, run, stop

--- server/server.go
+++ server/server.go
@@ -18,24 +18,141 @@ package server
 
 import (
 	"context"
+	"sync"
+	"sync/atomic"
 
+	"github.com/zaibyte/zbuf/extent"
+
+	"github.com/zaibyte/pkg/xlog"
+
+	"github.com/zaibyte/zbuf/vfs"
+
+	"github.com/zaibyte/pkg/xnet/xhttp"
+	"github.com/zaibyte/pkg/xrpc/xtcp"
 	"github.com/zaibyte/zbuf/server/config"
+	"github.com/zaibyte/zbuf/xio"
 )
 
 // Server is the zbuf server.
 type Server struct {
-	cfg *config.Config
+	isRunning int64
+
+	cfg    *config.Config
+	objSvr *xtcp.Server
+	opSvr  *xhttp.Server // Operator server.
+
+	nextDisk int64 // Next disk for creating new extent.
+	disks    []string
+
+	extenters *sync.Map // TODO better struct? sync.Map is not fast.
+
+	xioers map[string]*xioer // TODO should support concurrency.
+
+	ctx    context.Context
+	cancel func()
+	stopWg *sync.WaitGroup
+}
+
+type xioer struct {
+	stopWg *sync.WaitGroup
+
+	flusher      *xio.Flusher
+	flushJobChan chan *xio.FlushJob
+	getter       *xio.Getter
+	getJobChan   chan *xio.GetJob
+}
+
+func (x *xioer) start() {
+	for i := 0; i < xio.WriteThreadsPerDisk; i++ {
+		x.stopWg.Add(1)
+		go x.flusher.DoLoop()
+	}
+	for i := 0; i < xio.ReadThreadsPerDisk; i++ {
+		x.stopWg.Add(1)
+		go x.getter.DoLoop()
+	}
 }
 
 // Create creates a zbuf server.
 func Create(ctx context.Context, cfg *config.Config) (*Server, error) {
-	return nil, nil
+
+	s := &Server{}
+	s.cfg = cfg
+	s.ctx, s.cancel = context.WithCancel(ctx)
+	s.stopWg = new(sync.WaitGroup)
+
+	s.extenters = new(sync.Map)
+
+	disks, err := listDisks(vfs.DefaultFS, cfg.DataRoot)
+	if err != nil {
+		return nil, err
+	}
+	s.disks = disks
+
+	for _, disk := range disks {
+		flushJobChan := make(chan *xio.FlushJob, xio.DefaultWriteDepth)
+		flusher := &xio.Flusher{
+			Jobs:   flushJobChan,
+			Ctx:    s.ctx,
+			StopWg: s.stopWg,
+		}
+
+		getJobChan := make(chan *xio.GetJob, xio.DefaultReadDepth)
+		getter := &xio.Getter{
+			Jobs:   getJobChan,
+			Ctx:    s.ctx,
+			StopWg: s.stopWg,
+		}
+
+		s.xioers[disk] = &xioer{
+			flusher:      flusher,
+			flushJobChan: flushJobChan,
+			getter:       getter,
+			getJobChan:   getJobChan,
+		}
+	}
+
+	return s, nil
 }
 
+// TODO should start tsc.Calibrate()
 func (s *Server) Run() error {
+
+	err := s.objSvr.Start()
+	if err != nil {
+		return err
+	}
+	s.opSvr.Start()
+
+	for _, disk := range s.disks {
+		s.xioers[disk].start()
+	}
+
+	atomic.StoreInt64(&s.isRunning, 1)
+	xlog.Info("server is running")
+
 	return nil
 }
 
 func (s *Server) Close() {
 
+	if !atomic.CompareAndSwapInt64(&s.isRunning, 1, 0) {
+		// server is already closed
+		return
+	}
+
+	xlog.Info("closing server")
+
+	s.objSvr.Stop()
+	_ = s.opSvr.Close()
+
+	s.cancel()
+	s.stopWg.Wait()
+
+	s.extenters.Range(func(key, value interface{}) bool {
+		ext := value.(extent.Extenter)
+		_ = ext.Close()
+		return true
+	})
+	xlog.Info("server is closed")
 }
