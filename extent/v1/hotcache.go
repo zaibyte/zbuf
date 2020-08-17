98657a32c7991319fb2e9813edc5940e50ce3c0c
templexxx <lucas1x1x@gmail.com>
2020-08-17 22:31:27 +0800

    extent/v1: hotcache supports read for flushing

--- extent/v1/hotcache.go
+++ extent/v1/hotcache.go
@@ -82,13 +82,13 @@ func getHotCacheSegIDs(ids uint32) (idx, writeID, lastID uint16) {
 
 // write writes oid and its data into hotCache.
 // Only one goroutine could write at the same time.
-func (h *hotCache) write(nextSeg uint16, oid [16]byte, p []byte) (writeSeg uint16, offset, size int64) {
+func (h *hotCache) write(nextSeg uint16, oid [16]byte, p []byte) (ws uint16, offset, size int64) {
 
 	n := writeSpace(int64(len(p)))
 	idx, writeSeg, _ := getHotCacheSegIDs(atomic.LoadUint32(&h.ids))
 	c := h.scs[idx]
 
-	woff := atomic.LoadInt64(&c.writeOff)
+	woff := c.writeOff
 
 	if woff+n > h.size {
 		if nextSeg == sealedFlag { // No space for write.
@@ -100,13 +100,14 @@ func (h *hotCache) write(nextSeg uint16, oid [16]byte, p []byte) (writeSeg uint1
 		c = h.scs[idx]
 
 		writeSeg = nextSeg
+		c.writeOff = 0
 		woff = 0
 	}
 
 	copy(c.p[woff:], oid[:])
 	copy(c.p[woff+16:], p)
 
-	atomic.AddInt64(&c.writeOff, n)
+	c.writeOff += n
 
 	return writeSeg, woff, n
 }
@@ -138,10 +139,18 @@ func (h *hotCache) readData(addr uint32, w io.Writer, n uint32) uint32 {
 	return n
 }
 
-// writeSpace is the space will taken for the n bytes write.
-func writeSpace(n int64) int64 {
-	n = n + 16 // 16 is the oid length.
-	return alignSize(n, grainSize)
+// getBytesBySeg gets byte slice from cache by segment id.
+// This will be called in put loop process, it's safe and must have data, because put loop will block on it.
+func (h *hotCache) getBytesBySeg(seg uint16) []byte {
+	ids := atomic.LoadUint32(&h.ids)
+	idx, writeSeg, _ := getHotCacheSegIDs(ids)
+	var c *segCache
+	if seg == writeSeg {
+		c = h.scs[idx]
+	} else {
+		c = h.scs[(idx+1)&1]
+	}
+	return c.p
 }
 
 // addrToSeg finds which segment and its offset the address belongs to.
