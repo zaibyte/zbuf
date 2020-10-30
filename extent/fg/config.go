bfcd35e7b6c6289bcf8df3879d251fe02a6bdba8
xiangxi <xiangxi.xu@tesamc.com>
2020-10-30 17:44:37 +0800

    extent/fg: basic config functions

--- extent/fg/config.go
+++ extent/fg/config.go
@@ -1,5 +1,12 @@
 package fg
 
+import (
+	"time"
+
+	"g.tesamc.com/IT/zaipkg/config"
+	"g.tesamc.com/IT/zbuf/xio"
+)
+
 // There are default configs for fg.
 const (
 	defaultFlushDelay = -1 // Flush immediately. Rely on disk latency.
@@ -7,3 +14,25 @@ const (
 	defaultGetPending = 1024
 	defaultGetThread  = 4
 )
+
+type ExtentConfig struct {
+	Path         string
+	SegmentSize  int64
+	SizePerWrite int64
+	FlushDelay   time.Duration
+	GetThread    int
+	GetPending   int
+	PutPending   int
+	InsertOnly   bool
+}
+
+func (cfg *ExtentConfig) adjust() {
+	config.Adjust(&cfg.SegmentSize, defaultSegmentSize)
+	config.Adjust(&cfg.PutPending, defaultPutPending)
+	if cfg.FlushDelay == 0 {
+		cfg.FlushDelay = defaultFlushDelay
+	}
+	config.Adjust(&cfg.SizePerWrite, xio.DefaultSizePerWrite)
+	config.Adjust(&cfg.GetPending, defaultGetPending)
+	config.Adjust(&cfg.GetThread, defaultGetThread)
+}
--- extent/fg/extent.go
+++ extent/fg/extent.go
@@ -38,8 +38,6 @@ import (
 	"sync"
 	"time"
 
-	"g.tesamc.com/IT/zaipkg/config"
-
 	"g.tesamc.com/IT/zaipkg/xbytes"
 	"g.tesamc.com/IT/zaipkg/xlog"
 	"g.tesamc.com/IT/zbuf/vfs"
@@ -66,28 +64,6 @@ type Extent struct {
 
 	stopChan chan struct{}
 	stopWg   sync.WaitGroup
-}
-
-type ExtentConfig struct {
-	Path         string
-	SegmentSize  int64
-	SizePerWrite int64
-	FlushDelay   time.Duration
-	GetThread    int
-	GetPending   int
-	PutPending   int
-	InsertOnly   bool
-}
-
-func (cfg *ExtentConfig) adjust() {
-	config.Adjust(&cfg.SegmentSize, defaultSegmentSize)
-	config.Adjust(&cfg.PutPending, defaultPutPending)
-	if cfg.FlushDelay == 0 {
-		cfg.FlushDelay = defaultFlushDelay
-	}
-	config.Adjust(&cfg.SizePerWrite, xio.DefaultSizePerWrite)
-	config.Adjust(&cfg.GetPending, defaultGetPending)
-	config.Adjust(&cfg.GetThread, defaultGetThread)
 }
 
 // Create a new extent.
