d7f3e8aeade3de1a7e74948863a264289335f872
xiangxi <xiangxi.xu@tesamc.com>
2020-12-31 17:15:17 +0800

    xio/dqueue: basic configs

--- xio/dqueue/dqueue.go
+++ xio/dqueue/dqueue.go
@@ -10,82 +10,53 @@ import (
 
 // DiskQueue is I/O queue each disk will have on.
 type DiskQueue struct {
-	// The maximum number of concurrent rpc calls the server may perform.
-	// Default is DefaultConcurrency.
-	Concurrency int
-
-	// The maximum number of pending responses in the queue.
-	// Default is DefaultPendingMessages.
-	PendingResponses int
-
-	// Size of send buffer per each underlying connection in bytes.
-	// Default is DefaultBufferSize.
-	SendBufferSize int
-
-	// Size of recv buffer per each underlying connection in bytes.
-	// Default is DefaultBufferSize.
-	RecvBufferSize int
-
-	// The maximum delay between response flushes to clients.
-	//
-	// Negative values lead to immediate requests' sending to the client
-	// without their buffering. This minimizes rpc latency at the cost
-	// of higher CPU and network usage.
-	//
-	// Default is DefaultFlushDelay.
-	FlushDelay time.Duration
+	cfg *Config
 
 	ctx    context.Context
 	stopWg *sync.WaitGroup
 }
 
 const (
-	DefaultReadIODepth  = 64
-	DefaultWriteIODepth = 4
+	DefaultIODepth         = 64
+	DefaultPending         = 1024
+	DefaultWriteBufferSize = 128 * 1024
+	DefaultReadBufferSize  = 128 * 1024
+	DefaultFlushDelay      = 100 * time.Microsecond
 )
 
 // Config of DiskQueue.
 type Config struct {
-	// The maximum number of concurrent reads.
-	// Default is DefaultReadIODepth.
-	ReadIODepth int
-	// The maximum number of concurrent writes.
-	// Default is DefaultWriteIODepth.
-	WriteIODepth int
-
+	// The maximum number of concurrent read/write.
+	// Default is DefaultIODepth.
+	IODepth int
 	// The maximum number of pending requests in the queue.
-	//
-	// The number of pending requests should exceed the expected number
-	// of concurrent goroutines calling client's methods.
-	// Otherwise a lot of orpc.ErrRequestQueueOverflow errors may appear.
-	//
-	// Default is DefaultPendingMessages.
-	PendingRequests int
+	// Default is DefaultPending.
+	Pending int
 
-	// Maximum request time.
-	// Default value is DefaultRequestTimeout.
-	RequestTimeout time.Duration
-
-	// Size of send buffer per each underlying connection in bytes.
-	// Default value is DefaultClientSendBufferSize.
-	SendBufferSize int
-
-	// Size of recv buffer per each underlying connection in bytes.
-	// Default value is DefaultClientRecvBufferSize.
-	RecvBufferSize int
+	// Size of write buffer per writes in bytes.
+	// Default value is DefaultWriteBufferSize.
+	WriteBufferSize int
+	// Size of write buffer per reads in bytes.
+	// Default value is DefaultReadBufferSize.
+	ReadBufferSize int
 
 	// Delay between request flushes.
 	//
-	// Negative values lead to immediate requests' sending to the server
-	// without their buffering. This minimizes rpc latency at the cost
-	// of higher CPU and network usage.
+	// Negative values lead to immediate requests' sending to the filesystem
+	// without their buffering. This minimizes latency at the cost
+	// of higher CPU and disk usage.
 	//
 	// Default value is DefaultFlushDelay.
 	FlushDelay time.Duration
 }
 
-func New(ctx context.Context, stopWg *sync.WaitGroup) *DiskQueue {
-
+func New(ctx context.Context, stopWg *sync.WaitGroup, cfg *Config) *DiskQueue {
+	dq := &DiskQueue{
+		cfg:    cfg,
+		ctx:    ctx,
+		stopWg: stopWg,
+	}
+	return dq
 }
 
 func (d *DiskQueue) AddJob(j *xio.Request) {
