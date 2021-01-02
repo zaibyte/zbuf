550c3b2a5a256c85abd155ab33b817a60882708f
xiangxi <xiangxi.xu@g.tesamc.com>
2021-01-02 21:19:27 +0800

    xio/dqueue: add balanceWindow: That balancing is expected to happen over a specific time window

--- xio/dqueue/sched.go
+++ xio/dqueue/sched.go
@@ -4,6 +4,9 @@ import (
 	"context"
 	"sync"
 	"sync/atomic"
+	"time"
+
+	"github.com/templexxx/tsc"
 )
 
 type Scheduler struct {
@@ -21,17 +24,36 @@ func NewScheduler(ctx context.Context, stopWg *sync.WaitGroup, dqueue *DiskQueue
 	}
 }
 
+// That balancing is expected to happen over a specific time window,
+// default is 100ms.
+const balanceWindow = int64(100 * time.Millisecond)
+
 func (s *Scheduler) FindRunnableLoop() {
 	defer s.stopWg.Done()
 
 	ctx, cancel := context.WithCancel(s.ctx)
 	defer cancel()
 
+	start := tsc.UnixNano()
 	for {
+
 		go func(c *int64) {
 			cost :=
 				atomic.AddInt64(c, cost)
 		}(costed)
+
+		now := tsc.UnixNano()
+		if now-start >= balanceWindow {
+			s.setCostedsZero()
+			start = now
+		}
 	}
 
+}
+
+// set all totalCosted zero after meet the balance window.
+func (s *Scheduler) setCostedsZero() {
+	for _, q := range s.dqueue.queues {
+		atomic.StoreInt64(&q.totalCost, 0)
+	}
 }
