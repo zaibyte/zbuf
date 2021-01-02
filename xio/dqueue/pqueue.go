63861cec432ebdee053902b3b0d3492c98c4dd1e
xiangxi <xiangxi.xu@g.tesamc.com>
2021-01-02 17:49:29 +0800

    xio/dqueue: Add NewPriorityQueue

--- xio/dqueue/pqueue.go
+++ xio/dqueue/pqueue.go
@@ -1,8 +1,18 @@
 package dqueue
 
+import "g.tesamc.com/IT/zbuf/xio"
+
 // PriorityQueue provides requests queue for a certain priority class.
 type PriorityQueue struct {
 	shares    uint64
 	totalCost uint64
 	requests  *ReqQueue
+}
+
+func NewPriorityQueue(shares uint64, pending int) *PriorityQueue {
+	return &PriorityQueue{
+		shares:    shares,
+		totalCost: 0,
+		requests:  &ReqQueue{queue: make(chan *xio.AsyncRequest, pending)},
+	}
 }
