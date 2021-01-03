96cfcaeab26c79793988aa2641dbb0e58a40c906
xiangxi <xiangxi.xu@g.tesamc.com>
2021-01-03 14:38:35 +0800

    xio/dqueue: using float64 as total cost

--- xio/dqueue/pqueue.go
+++ xio/dqueue/pqueue.go
@@ -1,18 +1,21 @@
 package dqueue
 
-import "g.tesamc.com/IT/zbuf/xio"
+import (
+	"g.tesamc.com/IT/zaipkg/xatomic"
+	"g.tesamc.com/IT/zbuf/xio"
+)
 
 // PriorityQueue provides requests queue for a certain priority class.
 type PriorityQueue struct {
 	shares    uint64
-	totalCost int64
+	totalCost *xatomic.Float64
 	requests  *ReqQueue
 }
 
 func NewPriorityQueue(shares uint64, pending int) *PriorityQueue {
 	return &PriorityQueue{
 		shares:    shares,
-		totalCost: 0,
+		totalCost: xatomic.NewFloat64(0),
 		requests:  &ReqQueue{queue: make(chan *xio.AsyncRequest, pending)},
 	}
 }
