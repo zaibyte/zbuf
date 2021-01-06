b131e324cfd182ed0a4617ee238f3698100ba096
xiangxi <xiangxi.xu@tesamc.com>
2021-01-06 08:51:06 +0800

    xio/sched: remove useless old scheduler testing

--- xio/sched/sched_test.go
+++ xio/sched/sched_test.go
@@ -3,7 +3,6 @@ package sched
 import (
 	"context"
 	"fmt"
-	"math/rand"
 	"sync"
 	"testing"
 	"time"
@@ -43,21 +42,13 @@ func TestCalcWaitCoeff(t *testing.T) {
 	}
 }
 
-// Test Scheduler could satisfy fair principle,
-// at the same time, high priority requests should be executed more.
-//
-// Try to test with different speeds of VFS.
-func TestScheduler_FindRunnableLoopIsFair(t *testing.T) {
-	cnt, obj, gc := testScheduler_FindRunnableLoopIsFair(1000, 64, 50,
-		100, 128*1024)
-	fmt.Println(cnt, obj, gc)
-}
-
 type reqCnt struct {
 	reqType uint64
 	cnt     int
 }
 
+// Test Scheduler could satisfy fair principle,
+// at the same time, high priority requests should be executed more.
 func TestSchedulerIsFairWithPriority(t *testing.T) {
 	testSchedulerIsFairWithPriority(1000, 64, 128*1024, []reqCnt{
 
@@ -117,69 +108,4 @@ func testSchedulerIsFairWithPriority(vfsSpeed, iodepth, reqSize int, reqCnts []r
 		}(rc)
 	}
 	wg2.Wait()
-}
-
-// TODO should control send request speed
-func testScheduler_FindRunnableLoopIsFair(totalSpeed, iodepth int, highRatio int, totalTime int64, reqSize int64) (
-	totalCnt, objDone, gcDone int) {
-
-	maxIOPS := int64(totalSpeed*1024*1024) / reqSize
-	sleepDuration := time.Duration(int64(time.Second) / (int64(float64(maxIOPS) / 0.5 / 256)))
-	fmt.Println(maxIOPS, sleepDuration)
-
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-	wg := new(sync.WaitGroup)
-	s := New(ctx, wg, &Config{
-		IODepth:     iodepth,
-		QueueConfig: &QueueConfig{},
-	})
-	wg.Add(1)
-	go s.FindRunnableLoop()
-
-	speed := totalSpeed / iodepth
-	sf := &vfs.SpeedFile{Speed: speed}
-	data := make([]byte, reqSize)
-
-	rand.Seed(tsc.UnixNano())
-
-	ter := time.NewTimer(time.Duration(totalTime * balanceWindow))
-
-	ars := make([]*xio.AsyncRequest, 0, 65536)
-	stopped := false
-	cnt := 0
-	for !stopped {
-		if cnt == 256 {
-			cnt = 0
-			time.Sleep(sleepDuration)
-		}
-		select {
-		case <-ter.C:
-			stopped = true
-			break
-		default:
-			totalCnt++
-			cnt++
-			var reqType uint64 = xio.ReqObjRead
-			if rand.Intn(100) >= highRatio {
-				reqType = xio.ReqGCRead
-			}
-			ar, err := s.DoAsync(reqType, sf, 0, data)
-			if err == nil {
-				ars = append(ars, ar)
-			}
-		}
-	}
-
-	for _, ar := range ars {
-		<-ar.Done
-		if ar.Err == nil {
-			if ar.Type == xio.ReqObjRead {
-				objDone++
-			} else {
-				gcDone++
-			}
-		}
-	}
-	return
 }
