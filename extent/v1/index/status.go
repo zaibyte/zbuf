a70aaba2ed7103b6687250e289f6a69bfe61077e
xiangxi <xiangxi.xu@tesamc.com>
2021-01-07 15:46:25 +0800

    extent/v1/index: add status functions

---
+++ extent/v1/index/status.go
@@ -0,0 +1,153 @@
+package index
+
+import (
+	"sync/atomic"
+)
+
+// status struct(uint64):
+// 64                                                                                  58
+// <------------------------------------------------------------------------------------
+// | is_running(1) | locked(1) | sealed(1) | is_scaling(1) | writable(1) | padding(1) |
+// 58                       0
+// <------------------------
+// | padding(26) | cnt(32) |
+//
+// is_running: [63], is running or not.
+// locked: [62], is locked or not.
+// sealed: [61], seal Index when there is an unexpected failure.
+// is_scaling: [60], Index is expanding/shrinking.
+// writable: [59], writable table index.
+// cnt: [0,32), count of added keys.
+
+// IsRunning returns Index is running or not.
+func (s *Index) IsRunning() bool {
+	sa := atomic.LoadUint64(&s.status)
+	return bitOne(sa, 63)
+}
+
+// close sets status closed.
+func (s *Index) close() {
+	sa := atomic.LoadUint64(&s.status)
+	sa = clrBit(sa, 63)
+	atomic.StoreUint64(&s.status, sa)
+}
+
+// lock tries to lock Index, return true if succeed.
+func (s *Index) lock() bool {
+	sa := atomic.LoadUint64(&s.status)
+	if isLocked(sa) {
+		return false // locked.
+	}
+
+	nsa := setBit(sa, 62)
+	return atomic.CompareAndSwapUint64(&s.status, sa, nsa)
+}
+
+// unlock unlocks Index, Index must be locked.
+func (s *Index) unlock() {
+	sa := atomic.LoadUint64(&s.status)
+	sa = clrBit(sa, 62)
+	atomic.StoreUint64(&s.status, sa)
+}
+
+func isLocked(sa uint64) bool {
+	return bitOne(sa, 62)
+}
+
+// create status when New a Index.
+func createStatus() uint64 {
+
+	return setBit(0, 63) // index isRunning.
+}
+
+// TODO how to deal with sealed. Should pause make bigger table and transfer all data
+// seal seals Index.
+// When there is no writable table setting Index sealed.
+func (s *Index) seal() {
+	sa := atomic.LoadUint64(&s.status)
+	sa = setBit(sa, 61)
+	atomic.StoreUint64(&s.status, sa)
+}
+
+// isSealed returns Index is sealed or not.
+func (s *Index) isSealed() bool {
+	sa := atomic.LoadUint64(&s.status)
+	return bitOne(sa, 61)
+}
+
+// scale sets Index sealed.
+// When Index is expanding/shrinking setting Index scaling.
+func (s *Index) scale() {
+	sa := atomic.LoadUint64(&s.status)
+	sa = setBit(sa, 60)
+	atomic.StoreUint64(&s.status, sa)
+}
+
+// isScaling returns Index is scaling or not.
+func (s *Index) isScaling() bool {
+	sa := atomic.LoadUint64(&s.status)
+	return bitOne(sa, 60)
+}
+
+// unScale sets Index scalable.
+func (s *Index) unScale() {
+	sa := atomic.LoadUint64(&s.status)
+	sa = clrBit(sa, 60)
+	atomic.StoreUint64(&s.status, sa)
+}
+
+// getWritableIdx gets writable table in Index.
+// 0 or 1.
+func (s *Index) getWritableIdx() uint8 {
+	sa := atomic.LoadUint64(&s.status)
+	return uint8((sa >> 59) & 1)
+}
+
+func getWritableIdxByStatus(sa uint64) uint8 {
+	return uint8((sa >> 59) & 1)
+}
+
+// setWritable sets writable table index.
+func (s *Index) setWritable(idx uint8) {
+	sa := atomic.LoadUint64(&s.status)
+	if idx == 0 {
+		sa = clrBit(sa, 59)
+	} else {
+		sa = setBit(sa, 59)
+	}
+	atomic.StoreUint64(&s.status, sa)
+}
+
+// addCnt adds Index count.
+func (s *Index) addCnt() {
+	atomic.AddUint64(&s.status, 1) // cnt is the lowest bits, just +1.
+}
+
+// delCnt minutes Index count.
+func (s *Index) delCnt() {
+	atomic.AddUint64(&s.status, ^uint64(0))
+}
+
+const cntMask = (1 << 32) - 1
+
+func (s *Index) getCnt() uint64 {
+	sa := atomic.LoadUint64(&s.status)
+	return sa & cntMask
+}
+
+// Index x[off] to 1.
+func setBit(x uint64, off uint64) uint64 {
+	x |= 1 << off
+	return x
+}
+
+// Return x[off] is 1 or not.
+func bitOne(x, off uint64) bool {
+	return (x>>off)&1 == 1
+}
+
+// Index x[off] to 0.
+func clrBit(x uint64, off uint64) uint64 {
+	x &= ^(1 << off)
+	return x
+}
---
+++ extent/v1/index/status_test.go
@@ -0,0 +1,151 @@
+package index
+
+import (
+	"sync/atomic"
+	"testing"
+)
+
+func TestBitsOperator(t *testing.T) {
+	var x uint64 = 1
+	for i := 0; i < 64; i++ {
+		r := setBit(x, uint64(i))
+		if r != 1<<i|x {
+			t.Fatal("mismatched")
+		}
+		if !bitOne(r, uint64(i)) {
+			t.Fatal("should be one")
+		}
+		r = setBit(x, uint64(i)) // Should have no impact.
+		if r != 1<<i|x {
+			t.Fatal("mismatched")
+		}
+		if !bitOne(r, uint64(i)) {
+			t.Fatal("should be one")
+		}
+		x = r
+	}
+
+	for i := 63; i >= 0; i-- {
+		r := clrBit(x, uint64(i))
+		if r != 1<<i-1 {
+			t.Fatal("mismatched")
+		}
+		if bitOne(r, uint64(i)) {
+			t.Fatal("should not be one")
+		}
+		r = clrBit(x, uint64(i))
+		if r != 1<<i-1 {
+			t.Fatal("mismatched")
+		}
+		if bitOne(r, uint64(i)) {
+			t.Fatal("should not be one")
+		}
+		x = r
+	}
+}
+
+func TestSet_IsRunning(t *testing.T) {
+
+	s, _ := New(0)
+	if !s.IsRunning() {
+		t.Fatal("should be running")
+	}
+}
+
+func TestSet_Close(t *testing.T) {
+
+	s, _ := New(0)
+	s.Close()
+	if s.IsRunning() {
+		t.Fatal("should be closed")
+	}
+	s.close()
+	if s.IsRunning() {
+		t.Fatal("should be closed")
+	}
+}
+
+func TestCreateStatusWritable(t *testing.T) {
+
+	s, _ := New(0)
+	if s.getWritableIdx() != 0 {
+		t.Fatal("writable table mismatched")
+	}
+}
+
+func TestSetWritable(t *testing.T) {
+
+	s, _ := New(0)
+	s.setWritable(1)
+	if s.getWritableIdx() != 1 {
+		t.Fatal("writable table mismatched")
+	}
+	s.setWritable(0)
+	if s.getWritableIdx() != 0 {
+		t.Fatal("writable table mismatched")
+	}
+}
+
+func TestSetLock(t *testing.T) {
+
+	s, _ := New(0)
+	if !s.lock() {
+		t.Fatal("lock should be succeed")
+	}
+
+	if s.lock() {
+		t.Fatal("should be locked")
+	}
+
+	sa := atomic.LoadUint64(&s.status)
+	if !isLocked(sa) {
+		t.Fatal("should be locked")
+	}
+
+	s.unlock()
+
+	sa = atomic.LoadUint64(&s.status)
+	if isLocked(sa) {
+		t.Fatal("should be unlocked")
+	}
+}
+
+func TestSet_Seal(t *testing.T) {
+
+	s, _ := New(0)
+	s.seal()
+	if !s.isSealed() {
+		t.Fatal("should be sealed")
+	}
+}
+
+func TestSet_Scale(t *testing.T) {
+
+	s, _ := New(0)
+	s.scale()
+	if !s.isScaling() {
+		t.Fatal("should be scaling")
+	}
+	s.unScale()
+	if s.isScaling() {
+		t.Fatal("should be scalable")
+	}
+}
+
+func TestSet_Cnt(t *testing.T) {
+
+	s, _ := New(0)
+	for i := 0; i < 1024; i++ {
+		if s.getCnt() != uint64(i) {
+			t.Fatal("add count mismatch")
+		}
+		s.addCnt()
+	}
+
+	for i := 1024; i > 0; i-- {
+		if s.getCnt() != uint64(i) {
+			t.Fatal("del count mismatch")
+		}
+		s.delCnt()
+	}
+}
