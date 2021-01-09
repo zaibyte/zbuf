44b763c3ead08bb738b2c3426688a3367898c931
xiangxi <xiangxi.xu@g.tesamc.com>
2021-01-09 09:53:18 +0800

    extent/v1/index: remove useless testing(prop & bench)

--- extent/v1/index/prop_test.go
+++ extent/v1/index/prop_test.go
@@ -5,8 +5,6 @@ import (
 	"fmt"
 	"math/rand"
 	"os"
-	"runtime"
-	"sync"
 	"testing"
 	"time"
 )
@@ -40,79 +38,6 @@ func TestMitFull(t *testing.T) {
 	}
 
 	printRets(rets)
-}
-
-func TestContainsPerfConcurrent(t *testing.T) {
-
-	if !IsPropEnabled() {
-		t.Skip("skip perf testing")
-	}
-
-	n := 1024 * 1024
-	s, err := New(n * 2) // Ensure there is enough space for Adding, avoiding scaling.
-	if err != nil {
-		t.Fatal(err)
-	}
-	for i := 0; i < n; i++ {
-		err := s.Add(uint64(i))
-		if err != nil {
-			t.Fatal(err)
-		}
-	}
-
-	gn := runtime.NumCPU()
-	wg := new(sync.WaitGroup)
-	wg.Add(gn)
-	start := time.Now().UnixNano()
-	for i := 0; i < gn; i++ {
-		go func() {
-			defer wg.Done()
-			for j := 0; j < n; j++ {
-				_ = s.Search(uint64(j)) // TODO Should use different order, in present, cache impact a lot.
-			}
-		}()
-	}
-	wg.Wait()
-	end := time.Now().UnixNano()
-	ops := float64(end-start) / float64(n*gn)
-	iops := float64(n*gn) / (float64(end-start) / float64(time.Second))
-	t.Logf("total op: %d, cost: %dns, thread: %d;"+
-		"contains perf: %.2f ns/op, %.2f op/s", n*gn, end-start, gn, ops, iops)
-}
-
-func TestContainsPerf(t *testing.T) {
-
-	if !IsPropEnabled() {
-		t.Skip("skip perf testing")
-	}
-
-	n := 1024 * 1024
-	s, err := New(n * 2)
-	if err != nil {
-		t.Fatal(err)
-	}
-	exp := n
-	for i := 1; i < n+1; i++ {
-		err := s.Add(uint64(i))
-		if err != nil {
-			exp--
-		}
-	}
-
-	start := time.Now().UnixNano()
-	has := 0
-	for i := 1; i < n+1; i++ {
-		if s.Search(uint64(i)) {
-			has++
-		}
-	}
-
-	if has != exp {
-		t.Fatal("contains mismatch", has, exp, n)
-	}
-	end := time.Now().UnixNano()
-	ops := float64(end-start) / float64(exp)
-	t.Logf("contains perf: %.2f ns/op, total: %d, failed: %d, ok rate: %.8f", ops, n, n-exp, float64(exp)/float64(n))
 }
 
 func testMitFull(cnt int) int {
