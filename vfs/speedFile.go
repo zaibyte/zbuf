c7b554803bd72aecd968bfb46698aabaed64eb5b
xiangxi <xiangxi.xu@tesamc.com>
2021-01-12 17:21:53 +0800

    vfs: fix stupid rename, speedFS should be speedFile, it's a file not filesystem

---
+++ vfs/speedFile.go
@@ -0,0 +1,50 @@
+package vfs
+
+import (
+	"os"
+	"time"
+)
+
+type SpeedFile struct {
+	Speed int // MB/s.
+}
+
+func (s *SpeedFile) Close() error {
+	panic("implement me")
+}
+
+func (s *SpeedFile) Read(p []byte) (n int, err error) {
+	panic("implement me")
+}
+
+func (s *SpeedFile) ReadAt(p []byte, off int64) (n int, err error) {
+
+	ns := s.calcSleepTime(len(p))
+	time.Sleep(ns)
+
+	return len(p), nil
+}
+
+func (s *SpeedFile) calcSleepTime(n int) time.Duration {
+	st := float64(n) / float64(s.Speed*1024*1024) // seconds.
+	return time.Duration(int64(st * float64(time.Second)))
+}
+
+func (s *SpeedFile) Write(p []byte) (n int, err error) {
+	panic("implement me")
+}
+
+func (s *SpeedFile) WriteAt(p []byte, off int64) (n int, err error) {
+	ns := s.calcSleepTime(len(p))
+	time.Sleep(ns)
+
+	return len(p), nil
+}
+
+func (s *SpeedFile) Stat() (os.FileInfo, error) {
+	panic("implement me")
+}
+
+func (s *SpeedFile) Sync() error {
+	panic("implement me")
+}
