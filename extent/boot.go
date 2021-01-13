1260da6b48df38ff8d5219021ae3ad3eae846326
xiangxi <xiangxi.xu@tesamc.com>
2021-01-13 18:00:03 +0800

    extent: BootSector struct

---
+++ extent/boot.go
@@ -0,0 +1,16 @@
+package extent
+
+import (
+	"g.tesamc.com/IT/zaipkg/directio"
+	"g.tesamc.com/IT/zbuf/vfs"
+)
+
+const BootSectorSize = directio.BlockSize
+
+// CreateBootSector writes down a data block at the head of extent as the bootstrap of this extent.
+// BootSector struct:
+// 0                                                              BootSectorSize
+// | extent_version(2B) | xdigest(4B) | random_data(BootSectorSize-6B) |
+func CreateBootSector(f vfs.File) error {
+
+}
