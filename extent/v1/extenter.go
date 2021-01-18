d00c2d50c22e483f90bf1f774265ba53d7ca1b83
xiangxi <xiangxi.xu@tesamc.com>
2021-01-18 16:13:03 +0800

    typo

--- extent/v1/extenter.go
+++ extent/v1/extenter.go
@@ -1,18 +1,18 @@
-//Extent on local file system:
-//.
-//├── <data_root>
-//│    ├── disk_<disk_id0>
-//│    ├── disk_<disk_id1>
-//│    └── disk_<disk_id2>
-//│         └── ext
-//│              ├── <ext_id0>
-//│              ├── <ext_id1>
-//│              └── <ext_id2>
-//│                      ├── boot-sector
-//│                      ├── header
-//│                      ├── <timestamp>.idx-snap
-//│                      ├── <start-end>.idx-wal
-//│                      └── segments
+// Extent on local file system:
+// .
+// ├── <data_root>
+// │    ├── disk_<disk_id0>
+// │    ├── disk_<disk_id1>
+// │    └── disk_<disk_id2>
+// │         └── ext
+// │              ├── <ext_id0>
+// │              ├── <ext_id1>
+// │              └── <ext_id2>
+// │                      ├── boot-sector
+// │                      ├── header
+// │                      ├── <timestamp>.idx-snap
+// │                      ├── <start-end>.idx-wal
+// │                      └── segments
 
 package v1
 
