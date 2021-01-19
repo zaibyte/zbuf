8f36910ed19c69a5e7674dd038b3764e926969ad
xiangxi <xiangxi.xu@tesamc.com>
2021-01-19 13:50:46 +0800

    extenv/v1: refine header struct(adding gc supports)

--- extent/v1/header.go
+++ extent/v1/header.go
@@ -1,10 +1,41 @@
 package v1
 
-import "g.tesamc.com/IT/zbuf/vfs"
+import (
+	"sync"
+
+	"g.tesamc.com/IT/zbuf/vfs"
+)
 
 // Header is extent.v1 header.
 type Header struct {
-	f vfs.File
+	f vfs.File // Header will open a file, and keeping it opening until close.
+
+	// This part will be persisted to disk.
+	segSize int64
+	// Using a lock here won't break down performance,
+	// in some situation, it may improve performance, e.g. traversing segStates for header.Store,
+	// if we are using atomic, we have to load it one by one and copy it to a new allocated slice,
+	// using a lock could write done the states directly.
+	// And as a Header in extent, there won't be more than one thread to update Header,
+	// so the lock operation is just a lock instruction & an atomic compare, it won't be a slow lock
+	// which is waiting for wake up.
+	rwLock    *sync.RWMutex
+	segStates []byte  // 256 * 1B = 256B
+	sealedTS  []int64 // 256 * 8B = 2048B
+	// GC cursor updates every 1024 grains.
+	gcSrcCursor uint32 // GC source uid.GrainSize.
+	gcDstCursor uint32 // GC destination uid.GrainSize.
+
+	// This part will just keep in memory, because the changes of them are too frequently,
+	// and the fields could be reconstructed without persistence.
+	// segRemoved is the total size of removed objects in this segment.
+	segRemoved []uint32
+}
+
+func CreateHeader(extDir string) *Header {
+	h := new(Header)
+
+	return h
 }
 
 func (h *Header) Load() error {
