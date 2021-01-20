18b51aac1af173098306f89d185c9b408cf5d26e
xiangxi <xiangxi.xu@tesamc.com>
2021-01-20 09:22:36 +0800

    xio: add NopScheduler

--- xio/sched.go
+++ xio/sched.go
@@ -18,3 +18,25 @@ type Scheduler interface {
 	// If timeout = 0, using DefaultTimeout.
 	DoTimeout(reqType uint64, f vfs.File, offset int64, d []byte, timeout time.Duration) error
 }
+
+// NopScheduler wraps Scheduler but actually no scheduler working just write/read directly.
+type NopScheduler struct {
+}
+
+func (s *NopScheduler) DoAsync(reqType uint64, f vfs.File, offset int64, d []byte) (ar *AsyncRequest, err error) {
+
+	if IsReqRead(reqType) {
+		_, err = f.ReadAt(d, offset)
+		return nil, err
+	}
+
+	_, err = f.WriteAt(d, offset)
+
+	// TODO should I sync here?
+	return nil, err
+}
+
+func (s *NopScheduler) DoTimeout(reqType uint64, f vfs.File, offset int64, d []byte, timeout time.Duration) error {
+	_, err := s.DoAsync(reqType, f, offset, d)
+	return err
+}
