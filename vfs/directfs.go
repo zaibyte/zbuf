8b274ed1220daedb42ac44a8078f9a006c3fe270
xiangxi <xiangxi.xu@g.tesamc.com>
2021-01-31 11:14:30 +0800

    vfs: DirectFS will create file & sync the directory
    The caller won't do the sync & clean up outside now.

--- vfs/directfs.go
+++ vfs/directfs.go
@@ -40,8 +40,19 @@ func (fs directFS) OpenDir(name string) (File, error) {
 	return os.OpenFile(name, syscall.O_CLOEXEC, 0)
 }
 
+// Create creates a new file read/write, and sync the directory.
+// If failed, trying to remove dirty file.
 func (directFS) Create(name string) (File, error) {
-	return directio.OpenFile(name, os.O_RDWR|os.O_CREATE|os.O_TRUNC|syscall.O_CLOEXEC|fnc.O_NOATIME, 0666)
+	f, err := directio.OpenFile(name, os.O_RDWR|os.O_CREATE|os.O_TRUNC|syscall.O_CLOEXEC|fnc.O_NOATIME, 0666)
+	if err != nil {
+		return nil, err
+	}
+	err = SyncDir(DirectFS, filepath.Dir(name))
+	if err != nil {
+		_ = os.Remove(name)
+		return nil, err
+	}
+	return f, nil
 }
 
 func (directFS) Link(oldname, newname string) error {
