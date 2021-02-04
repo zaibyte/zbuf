3d27b19e84521b9650870d86218adf9dcc501e88
xiangxi <xiangxi.xu@tesamc.com>
2021-02-04 18:03:31 +0800

    extent/v1: refinehandleIOError logic: put sync header in Close

---
+++ extent/v1/errors.go
@@ -0,0 +1,53 @@
+package v1
+
+import (
+	"errors"
+	"fmt"
+	"syscall"
+
+	"g.tesamc.com/IT/zaipkg/diskutil"
+	"g.tesamc.com/IT/zaipkg/orpc"
+	"g.tesamc.com/IT/zaipkg/xlog"
+	"g.tesamc.com/IT/zproto/pkg/metapb"
+)
+
+// handleDMUError handles DMU error.
+func (e *Extenter) handleDMUError(err error) {
+
+}
+
+// handleIOError handles I/O error.
+func (e *Extenter) handleIOError(err error) {
+
+	isGhost := false
+	if diskutil.IsBroken(err) {
+		var ferr error
+		if errors.Is(err, syscall.EIO) {
+			ferr = e.fastDiskHealthCheck()
+		}
+		if ferr != nil {
+			xlog.Error(fmt.Sprintf("disk: %d is broken: %s", e.diskInfo.PbDisk.Id, err.Error()))
+			e.diskInfo.SetState(metapb.DiskState_Disk_Broken, false)
+		} else {
+			isGhost = true
+		}
+	}
+
+	isFull := false
+	state := metapb.ExtentState_Extent_Broken
+	if errors.Is(err, orpc.ErrExtentFull) {
+		isFull = true
+		state = metapb.ExtentState_Extent_Full
+	}
+	if errors.Is(err, orpc.ErrChecksumMismatch) || isGhost {
+		state = metapb.ExtentState_Extent_Ghost
+		isGhost = true
+	}
+	xlog.Error(fmt.Sprintf("extent: %d is %s: %s", e.info.PbExt.Id, state.String(), err.Error()))
+
+	e.info.SetState(state, false)
+	e.cleanPendingUpdates(err, isFull)
+	if state == metapb.ExtentState_Extent_Broken || isGhost {
+		_ = e.Close()
+	}
+}
