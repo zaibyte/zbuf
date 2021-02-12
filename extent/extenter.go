317ee0c4671a831161b77f86c7048d8cd51b0206
xiangxi <xiangxi.xu@g.tesamc.com>
2021-02-12 08:47:33 +0800

    extent: add Zai.Client in CreateParams

--- extent/extenter.go
+++ extent/extenter.go
@@ -4,10 +4,9 @@ import (
 	"context"
 	"sync"
 
+	zai "g.tesamc.com/IT/zai/client"
 	"g.tesamc.com/IT/zaipkg/orpc"
-
 	"g.tesamc.com/IT/zbuf/vdisk"
-
 	"g.tesamc.com/IT/zbuf/vfs"
 	"g.tesamc.com/IT/zproto/pkg/metapb"
 )
@@ -33,22 +32,21 @@ var AvailVersions = []uint16{Version1, VersionTest}
 
 // CreateParams are the params for creating an extent.
 type CreateParams struct {
+	Zai        zai.Client
 	InstanceID uint32
 	DiskID     uint32
 	ExtID      uint32
 	DiskInfo   *vdisk.Info
-	IsClone    bool
-	State      metapb.ExtentState
-	ObjCount   uint32 // Hint clone extent DMU capacity.
+	CloneJob   *metapb.CloneJob
 }
 
 // Creator could create/open extenter.
 type Creator interface {
 	// Create creates Extenter which not existed.
 	// dir is extent dir.
-	Create(ctx context.Context, wg *sync.WaitGroup, fs vfs.FS, dir string, params CreateParams) (Extenter, error)
+	Create(ctx context.Context, wg *sync.WaitGroup, fs vfs.FS, extDir string, params CreateParams) (Extenter, error)
 	// Load loads an existed Extenter.
-	Load(ctx context.Context, wg *sync.WaitGroup, fs vfs.FS, dir string, params CreateParams) (Extenter, error)
+	Load(ctx context.Context, wg *sync.WaitGroup, fs vfs.FS, extDir string, params CreateParams) (Extenter, error)
 	// GetSize gets the space size will be taken by the extent which will be created.
 	GetSize() uint64
 }
