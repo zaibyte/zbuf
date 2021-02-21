572ced36f7f302f81a75748637cd909802e2e7cd
xiangxi <xiangxi.xu@g.tesamc.com>
2021-02-21 18:25:09 +0800

    extent/v1: TryClone done

--- extent/v1/clone.go
+++ extent/v1/clone.go
@@ -2,18 +2,17 @@ package v1
 
 import (
 	"bytes"
+	"encoding/binary"
 	"errors"
 	"fmt"
 	"time"
 
-	"g.tesamc.com/IT/zbuf/extent"
-
-	"g.tesamc.com/IT/zaipkg/orpc"
-
 	"g.tesamc.com/IT/zaipkg/config/settings"
+	"g.tesamc.com/IT/zaipkg/orpc"
 	"g.tesamc.com/IT/zaipkg/uid"
 	"g.tesamc.com/IT/zaipkg/xerrors"
 	"g.tesamc.com/IT/zaipkg/xlog"
+	"g.tesamc.com/IT/zbuf/extent"
 	"g.tesamc.com/IT/zproto/pkg/metapb"
 )
 
@@ -40,31 +39,62 @@ func (e *Extenter) TryClone(job *metapb.CloneJob) {
 		MaxSleep: 15 * time.Second,
 	}
 
+	objDataBuf := bytes.NewBuffer(make([]byte, settings.MaxObjectSize))
+
 	for done < grains*uid.GrainSize {
 
 		var n int64
 		var err error
-		for i := 0; ; i++ {
+		for i := 0; ; i++ { // Keeping trying.
 			oidsBody.Reset() // Avoiding dirty read.
-			n, err = e.zai.GetObj(oidsOID, oidsBody, int64(done), int64(cloneOIDsBufSize), 3*time.Second)
+			n, err = e.zai.GetObj(oidsOID, oidsBody, int64(done), int64(cloneOIDsBufSize), true, 3*time.Second)
 			if err != nil {
 				xlog.Warn(xerrors.WithMessage(err, fmt.Sprintf("failed to get clone job oids_oid: %d", oidsOID)).Error())
 				if errors.Is(err, orpc.ErrReplicasCollapse) {
 					extent.SetCloneJobState(job, metapb.CloneJobState_CloneJob_Failed, false)
 					return
 				}
-				time.Sleep(retry.GetSleepDuration(i+1, cloneOIDsBufSize))
-				continue
+				if orpc.CouldRetry(err) {
+					time.Sleep(retry.GetSleepDuration(i+1, cloneOIDsBufSize))
+					continue
+				} else {
+					extent.SetCloneJobState(job, metapb.CloneJobState_CloneJob_Failed, false)
+					return
+				}
 			}
 			break
 		}
 
-		oidsBody.Bytes()
+		oids := oidsBody.Bytes()
 
-		// TODO deal with oids_set which got in last request.
+		for i := 0; i < len(oids)/8; i++ {
+			oid := binary.LittleEndian.Uint64(oids[i*8 : i*8+8])
+			_, err = e.zai.GetObj(oid, objDataBuf, 0, settings.MaxObjectSize, true, 3*time.Second)
+			if err != nil {
+				xlog.Warn(xerrors.WithMessage(err, fmt.Sprintf("failed to get clone job oid: %d", oid)).Error())
+				if errors.Is(err, orpc.ErrNotFound) {
+					continue
+				}
+
+				if errors.Is(err, orpc.ErrReplicasCollapse) {
+					extent.SetCloneJobState(job, metapb.CloneJobState_CloneJob_Failed, false)
+					return
+				}
+			}
+			err = e.PutObj(0, oid, objDataBuf.Bytes(), true)
+			if err != nil {
+				xlog.Error(xerrors.WithMessage(err, fmt.Sprintf("failed to put object: %d when clone", oid)).Error())
+				return
+			}
+			e.rwMutex.Lock()
+			e.header.nvh.CloneJob.DoneCnt += 1
+			e.rwMutex.Unlock()
+		}
 
 		done += uint32(n)
 
 	}
 
+	extent.SetCloneJobState(job, metapb.CloneJobState_CloneJob_Done, false)
+	e.info.SetState(metapb.ExtentState_Extent_ReadWrite, false)
 }
