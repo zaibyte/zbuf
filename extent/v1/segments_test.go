82d7317da0f01208bd9e135d7e260d68abefe117
xiangxi <xiangxi.xu@tesamc.com>
2021-02-23 13:59:26 +0800

    extent/v1: testing of functions of segments

--- extent/v1/segments_test.go
+++ extent/v1/segments_test.go
@@ -1,1 +1,19 @@
 package v1
+
+import (
+	"testing"
+
+	"g.tesamc.com/IT/zbuf/extent/v1/dmu"
+)
+
+func TestAddrToSeg(t *testing.T) {
+	for i := 0; i < segmentCnt; i++ {
+		for j := dmu.AlignSize; j < dmu.AlignSize*8; j *= 2 {
+			for k := 0; k < j/dmu.AlignSize; k++ {
+				if addrToSeg(uint32(k), int64(j)) != 0 {
+					t.Fatal("addrToSeg mismatched")
+				}
+			}
+		}
+	}
+}
