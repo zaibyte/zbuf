c110b9498042434a2bb425c7e5ee1d131411382a
xiangxi <xiangxi.xu@g.tesamc.com>
2021-03-01 20:04:51 +0800

    extent/v1: add testing of get max DMU snapshot size

--- extent/v1/dmu_test.go
+++ extent/v1/dmu_test.go
@@ -1,3 +1,16 @@
 package v1
 
+import (
+	"testing"
+
+	"g.tesamc.com/IT/zaipkg/xmath"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetMaxDMUSnapSize(t *testing.T) {
+
+	assert.Equal(t, 143.47, xmath.Round(getMaxDMUSnapSize(uint64(defaultSegmentSize), defaultReservedSeg)/1024/1024, 2))
+}
+
 // TODO test empty snap create & load
