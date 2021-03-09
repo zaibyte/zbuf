f7db4a20668b0e66500a78b4f591868dbe738e6e
templexxx <lucas1x1x@gmail.com>
2021-03-09 13:05:11 +0800

    extent/v1: mov setState to extenter.go

--- extent/v1/extenter.go
+++ extent/v1/extenter.go
@@ -22,8 +22,11 @@ import (
 	"fmt"
 	"sync"
 	"sync/atomic"
+	"syscall"
 	"time"
 	"unsafe"
+
+	"g.tesamc.com/IT/zaipkg/diskutil"
 
 	"g.tesamc.com/IT/zaipkg/directio"
 
@@ -461,4 +464,33 @@ func (e *Extenter) traverseDirtyDeleteWAL() error {
 // cleanDirtyUpdates set dirtyUpdates 0 directly.
 func (e *Extenter) cleanDirtyUpdates() {
 	atomic.StoreInt64(&e.dirtyUpdates, 0)
+}
+
+// setState sets Extenter state by err.
+func (e *Extenter) setState(err error) {
+
+	if err == nil {
+		return
+	}
+
+	old := e.info.GetState()
+	var state metapb.ExtentState
+	if diskutil.IsBroken(err) {
+		xlog.Error(fmt.Sprintf("disk: %d is broken: %s", e.diskInfo.PbDisk.Id, err.Error()))
+		_ = e.diskInfo.SetState(metapb.DiskState_Disk_Broken, false)
+		state = metapb.ExtentState_Extent_Broken
+	} else if errors.Is(err, syscall.EIO) {
+		state = metapb.ExtentState_Extent_Broken
+	} else if errors.Is(err, orpc.ErrExtentFull) {
+		state = metapb.ExtentState_Extent_Full
+	} else if errors.Is(err, orpc.ErrChecksumMismatch) { // Silent corruption.
+		state = metapb.ExtentState_Extent_Ghost
+	} else {
+		state = old
+	}
+
+	if state != old {
+		xlog.Error(fmt.Sprintf("extent: %d is %s: %s", e.info.PbExt.Id, state.String(), err.Error()))
+	}
+	e.info.SetState(state, false)
 }
