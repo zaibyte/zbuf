b9e558b1fee55457b5693dea090d0c9c469e5bd6
templexxx <lucas1x1x@gmail.com>
2021-03-09 13:53:13 +0800

    extent/v1: testing of dmu snapshot entry making & parsing

--- extent/v1/dmu_test.go
+++ extent/v1/dmu_test.go
@@ -3,6 +3,8 @@ package v1
 import (
 	"testing"
 
+	"g.tesamc.com/IT/zbuf/extent/v1/dmu"
+
 	"g.tesamc.com/IT/zaipkg/xmath"
 
 	"github.com/stretchr/testify/assert"
@@ -10,9 +12,24 @@ import (
 
 func TestGetMaxDMUSnapSize(t *testing.T) {
 
-	assert.Equal(t, 143.47, xmath.Round(getMaxDMUSnapSize(uint64(defaultSegmentSize), defaultReservedSeg)/1024/1024, 2))
+	assert.Equal(t, 143.47, xmath.Round(getMaxDMUSnapSize(uint64(defaultSegmentSize),
+		defaultReservedSeg)/1024/1024, 2))
 }
 
 func TestDMUSnapEntryMakeParse(t *testing.T) {
 
+	efs := dmu.GenEntriesFast(1024)
+	slotCnt := uint32(dmu.CalcSlotCnt(dmu.MinCap))
+	for _, ef := range efs {
+		e0, e1 := makeDMUSnapEntry(dmu.MakeEntry(
+			ef.Digest, 0, ef.Otype, ef.Grains, ef.Addr),
+			slotCnt, uint32(dmu.CalcSlot(int(slotCnt), ef.Digest)))
+
+		digest, otype, grains, addr := parseDmuSnapEntry(e0, e1)
+		assert.Equal(t, ef.Digest, digest)
+		assert.Equal(t, ef.Otype, otype)
+		assert.Equal(t, ef.Grains, grains)
+		assert.Equal(t, ef.Addr, addr)
+
+	}
 }
