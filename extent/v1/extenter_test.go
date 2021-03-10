d4242ae8ab4f8eec7e5891cb5aa25071b8d64e78
templexxx <lucas1x1x@gmail.com>
2021-03-10 17:08:17 +0800

    extent/v1: add function to createTestExtenter

--- extent/v1/extenter_test.go
+++ extent/v1/extenter_test.go
@@ -1,1 +1,31 @@
 package v1
+
+import (
+	"context"
+	"io/ioutil"
+	"os"
+
+	"g.tesamc.com/IT/zbuf/extent"
+)
+
+func createTestExtenter(cfg *Config) (ext *Extenter, err error) {
+
+	extDir, err := ioutil.TempDir(os.TempDir(), "ext.v1.creator")
+	if err != nil {
+		return nil, err
+	}
+
+	c := makeTestCreator(cfg)
+
+	e, err := c.Create(context.Background(), extDir, extent.CreateParams{
+		InstanceID: 1,
+		DiskID:     1,
+		ExtID:      1,
+		DiskInfo:   nil,
+		CloneJob:   nil,
+	})
+	if err != nil {
+		return nil, err
+	}
+	return e.(*Extenter), nil
+}
