ea558abfdf1d08abf6c1b833698d330f25421bf9
templexxx <lucas1x1x@gmail.com>
2021-03-11 14:20:09 +0800

    extent/v1: add memZai for satisify the needs of Zai in testing

--- extent/v1/extenter_test.go
+++ extent/v1/extenter_test.go
@@ -2,9 +2,15 @@ package v1
 
 import (
 	"context"
+	"io"
 	"io/ioutil"
 	"os"
+	"sync"
+	"time"
 
+	"g.tesamc.com/IT/zaipkg/orpc"
+	"g.tesamc.com/IT/zaipkg/uid"
+	"g.tesamc.com/IT/zaipkg/xdigest"
 	_ "g.tesamc.com/IT/zaipkg/xlog/xlogtest"
 	"g.tesamc.com/IT/zbuf/extent"
 )
@@ -29,4 +35,62 @@ func createTestExtenter(cfg *Config) (ext *Extenter, err error) {
 		return nil, err
 	}
 	return e.(*Extenter), nil
+}
+
+// memZai is a Zai Client built for testing purpose
+// which using memory for mocking Zai Box features.
+type memZai struct {
+	sync.RWMutex
+	// boxID & extID here for building oid,
+	// memZai is only made for ext.v1 testing, it's okay to share the same extID.
+	boxID   uint32
+	extID   uint32
+	oidData map[uint64][]byte
+}
+
+func (m *memZai) PutObj(objData io.Reader, timeout time.Duration) (oid uint64, read int64, err error) {
+
+	m.Lock()
+	defer m.Unlock()
+
+	d, err := ioutil.ReadAll(objData)
+	if err != nil {
+		return 0, 0, err
+	}
+
+	oid = uid.MakeOID(m.boxID, uint32(uid.GetGroupID(m.extID)), uint32(len(d)/uid.GrainSize), xdigest.Sum32(d), uid.NormalObj)
+
+	m.oidData[oid] = d
+
+	return oid, int64(len(d)), nil
+}
+
+func (m *memZai) GetObj(oid uint64, objData io.Writer, offset, n int64, isClone bool, timeout time.Duration) (written int64, err error) {
+	m.RLock()
+	defer m.RUnlock()
+
+	d, ok := m.oidData[oid]
+	if !ok {
+		return 0, orpc.ErrNotFound
+	}
+
+	exp := d[offset : offset+n]
+	w, err := objData.Write(exp)
+	return int64(w), err
+}
+
+func (m *memZai) DeleteObj(oid uint64, timeout time.Duration) error {
+	m.Lock()
+	defer m.Unlock()
+
+	delete(m.oidData, oid)
+	return nil
+}
+
+func (m *memZai) UpdateObj(oid uint64, offset, newData io.Reader) (newOid uint64, read int64, err error) {
+	return 0, 0, err
+}
+
+func (m *memZai) Close() {
+	return
 }
