b961fe954c4e79a02fc8ad9edfbbcba324730ff3
templexxx <lucas1x1x@gmail.com>
2021-03-16 15:40:21 +0800

    tools/extperf: prepare done

--- tools/extperf/extperf/prepare.go
+++ tools/extperf/extperf/prepare.go
@@ -6,19 +6,13 @@ import (
 	"sync"
 
 	zai "g.tesamc.com/IT/zai/client"
-	"g.tesamc.com/IT/zbuf/vfs"
-
+	"g.tesamc.com/IT/zaipkg/uid"
+	"g.tesamc.com/IT/zaipkg/xdigest"
 	"g.tesamc.com/IT/zbuf/extent"
 	v1 "g.tesamc.com/IT/zbuf/extent/v1"
-
-	"github.com/templexxx/xhex"
-	"github.com/zaibyte/pkg/xbytes"
-	"github.com/zaibyte/pkg/xstrconv"
-
-	"g.tesamc.com/IT/zaipkg/uid"
+	"g.tesamc.com/IT/zbuf/vfs"
 
 	"github.com/templexxx/tsc"
-	"github.com/zaibyte/pkg/xdigest"
 )
 
 func (r *Runner) createExtents() (err error) {
@@ -31,6 +25,8 @@ func (r *Runner) createExtents() (err error) {
 
 	cfg := v1.GetDefaultConfig()
 	cfg.UpdatesPending = r.cfg.PutPending
+	cfg.Development = true
+	cfg.UpdateOrInsert = true
 	c := v1.NewCreator(cfg, r.disks, fs, new(zai.NopClient), 1)
 
 	idx := 0
@@ -56,67 +52,30 @@ func (r *Runner) createExtents() (err error) {
 	return nil
 }
 
-var coldObj []byte
-var coldDigest uint32
-var coldOID [16]byte
-var coldData = xbytes.GetNBytes(defaultObjSize)
+var testObj []byte
+var testObjOID uint64
+var testObjDigest uint32
 
-var hotObj []byte
-var hotDigest uint32
-var hotOID [16]byte
-var hotData = xbytes.GetNBytes(defaultObjSize)
-
-func fillObjData() {
+func randFillObj(nKB int64) {
 	rand.Seed(tsc.UnixNano())
 
-	coldObj = make([]byte, defaultObjSize)
-	rand.Read(coldObj)
-	coldData.Write(coldObj)
-	coldDigest = xdigest.Sum32(coldObj)
-	_, oid := uid.MakeOID(1, 1, coldDigest, defaultObjSize, uid.NormalObj)
-	xhex.Decode(coldOID[:], xstrconv.ToBytes(oid))
-
-	hotObj = make([]byte, defaultObjSize)
-	rand.Read(hotObj)
-	hotData.Write(hotObj)
-	hotDigest = xdigest.Sum32(hotObj)
-	_, oid = uid.MakeOID(1, 1, hotDigest, defaultObjSize, uid.NormalObj)
-	xhex.Decode(hotOID[:], xstrconv.ToBytes(oid))
+	testObj = make([]byte, nKB*1024)
+	rand.Read(testObj)
+	testObjDigest = xdigest.Sum32(testObj)
+	testObjOID = uid.MakeOID(1, 1, uint32(nKB/4), testObjDigest, uid.NormalObj)
 }
 
-// Ensure every extent has one hot object
-func (r *Runner) putHot() {
+// prepareRead ensure every extent has one object.
+func (r *Runner) prepareRead() {
 	var wg sync.WaitGroup
 	for i := 0; i < 8; i++ {
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
-			for j := 0; j < r.cfg.PrepareCnt; j++ {
-				for k := 0; k < len(r.putJobers); k++ {
-					ok, _ := r.putJobers[0].put(hotOID, hotData)
-					if !ok {
-						log.Fatal("prepare hot failed")
-					}
-				}
-			}
-		}()
-	}
-	wg.Wait()
-}
-
-// Ensure every extent has one cold object
-func (r *Runner) putCold() {
-	var wg sync.WaitGroup
-	for i := 0; i < 8; i++ {
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			for j := 0; j < r.cfg.PrepareCnt; j++ {
-				for k := 0; k < len(r.putJobers); k++ {
-					ok, _ := r.putJobers[0].put(coldOID, coldData)
-					if !ok {
-						log.Fatal("prepare cold failed")
-					}
+			for k := 0; k < len(r.putJobers); k++ {
+				ok, _ := r.putJobers[0].put(testObjOID, testObj)
+				if !ok {
+					log.Fatal("prepare objects failed")
 				}
 			}
 		}()
