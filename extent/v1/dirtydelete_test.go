5643ee82e8af44afcf7adb5739e4c3929f4dfc92
templexxx <lucas1x1x@gmail.com>
2021-04-07 14:14:03 +0800

    extent/v1: moving codes of dirty delete out of io.go

---
+++ extent/v1/dirtydelete_test.go
@@ -0,0 +1,146 @@
+package v1
+
+import (
+	"encoding/binary"
+	"testing"
+
+	"g.tesamc.com/IT/zaipkg/uid"
+	"github.com/stretchr/testify/assert"
+	"github.com/templexxx/tsc"
+)
+
+// TestDirtyDelete tries to test the filter inside the dirtyDelete struct.
+func TestDirtyDelete(t *testing.T) {
+	db := newDirtyDelete(nil)
+	cnt := maxDirtyDelBatch + maxDirtyDelOne
+	digests := uid.GenRandDigests(cnt * 10)
+
+	buf := make([]byte, 4)
+
+	added := digests[:cnt]
+	for i := range added {
+		binary.LittleEndian.PutUint32(buf, added[i])
+		db.bf.Add(buf)
+	}
+
+	for i := range added {
+		binary.LittleEndian.PutUint32(buf, added[i])
+		if !db.bf.Test(buf) {
+			t.Fatal("must have")
+		}
+	}
+
+	miss := digests[cnt:]
+	falsePositiveCnt := 0
+	for i := range miss {
+		binary.LittleEndian.PutUint32(buf, miss[i])
+		if db.bf.Test(buf) {
+			falsePositiveCnt++
+		}
+	}
+
+	if float64(falsePositiveCnt)/float64(cnt*9) > 0.04 {
+		t.Fatal("false positive is > 0.04")
+	}
+}
+
+func TestDeleteWALChunk(t *testing.T) {
+
+	cnt := 10
+	buf := make([]byte, delWALChunkMinSize*cnt)
+	digests := uid.GenRandDigests(cnt)
+	expTS := tsc.UnixNano()
+	for i := 0; i < cnt; i++ {
+		makeDelWALChunk(digests[i], expTS, buf[i*delWALChunkMinSize:])
+	}
+
+	for i := 0; i < cnt; i++ {
+		isEnd, ts, rdigests, n, err := readDelWALChunk(buf[i*delWALChunkMinSize:])
+		if err != nil {
+			t.Fatal(err)
+		}
+		assert.Equal(t, false, isEnd)
+		assert.Equal(t, expTS, ts)
+		assert.Equal(t, []uint32{digests[i]}, rdigests)
+		assert.Equal(t, int64(delWALChunkMinSize), n)
+	}
+}
+
+func TestDeleteBatchWALChunk(t *testing.T) {
+
+	cnt := maxDirtyDelBatch
+	buf := make([]byte, 128*1024) // Batch chunk won't > 128KB.
+	oids := uid.GenRandOIDs(cnt)
+	expTS := tsc.UnixNano()
+	expN := makeDelBatchWALChunk(oids, expTS, buf)
+
+	isEnd, ts, rdigests, n, err := readDelWALChunk(buf)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expDigest := make([]uint32, cnt)
+	for i := range oids {
+		expDigest[i] = uid.GetDigest(oids[i])
+	}
+	assert.Equal(t, false, isEnd)
+	assert.Equal(t, expTS, ts)
+	assert.Equal(t, expDigest, rdigests)
+	assert.Equal(t, expN, n)
+}
+
+func TestDeleteWALChunkMixed(t *testing.T) {
+	singleCnt0, singleCnt1 := maxDirtyDelOne/2, maxDirtyDelOne/2
+	batchCnt := maxDirtyDelBatch
+	buf := make([]byte, dirtyDeleteWALSize)
+	digests := uid.GenRandDigests(singleCnt0 + singleCnt1)
+	expTS0 := tsc.UnixNano()
+	for i := 0; i < singleCnt0; i++ {
+		makeDelWALChunk(digests[i], expTS0, buf[i*delWALChunkMinSize:])
+	}
+	oids := uid.GenRandOIDs(batchCnt)
+	expTSBatch := tsc.UnixNano()
+	batchN := makeDelBatchWALChunk(oids, expTSBatch, buf[singleCnt0*delWALChunkMinSize:])
+
+	expTS1 := tsc.UnixNano()
+	start := int64(singleCnt0*delWALChunkMinSize) + batchN
+	for i := 0; i < singleCnt1; i++ {
+		makeDelWALChunk(digests[singleCnt0+i], expTS1, buf[start+int64(i)*delWALChunkMinSize:start+(int64(i+1)*delWALChunkMinSize)])
+	}
+
+	var done int64 = 0
+	for i := 0; i < singleCnt0; i++ {
+		isEnd, ts, rdigests, n, err2 := readDelWALChunk(buf[done:])
+		assert.Nil(t, err2)
+		assert.Equal(t, isEnd, false)
+		assert.Equal(t, ts, expTS0)
+		assert.Equal(t, int64(delWALChunkMinSize), n)
+		assert.Equal(t, []uint32{digests[i]}, rdigests)
+		done += n
+	}
+
+	isEnd, ts, rdigests, n, err2 := readDelWALChunk(buf[done:])
+	assert.Nil(t, err2)
+	assert.Equal(t, isEnd, false)
+	assert.Equal(t, ts, expTSBatch)
+	assert.Equal(t, batchN, n)
+	expDigest := make([]uint32, batchCnt)
+	for i := range oids {
+		expDigest[i] = uid.GetDigest(oids[i])
+	}
+	assert.Equal(t, expDigest, rdigests)
+	done += n
+
+	for i := 0; i < singleCnt1; i++ {
+		isEnd, ts, rdigests, n, err2 = readDelWALChunk(buf[done:])
+		assert.Nil(t, err2)
+		assert.Equal(t, isEnd, false)
+		assert.Equal(t, ts, expTS1)
+		assert.Equal(t, int64(delWALChunkMinSize), n)
+		assert.Equal(t, []uint32{digests[singleCnt0+i]}, rdigests)
+		done += n
+	}
+
+	isEnd, _, _, _, _ = readDelWALChunk(buf[done:])
+	assert.True(t, isEnd)
+}
--- extent/v1/io.go
+++ extent/v1/io.go
@@ -9,8 +9,6 @@ import (
 	"sync"
 	"sync/atomic"
 
-	"g.tesamc.com/IT/zbuf/vfs"
-
 	"g.tesamc.com/IT/zbuf/xio"
 
 	"g.tesamc.com/IT/zaipkg/directio"
@@ -25,55 +23,7 @@ import (
 	"g.tesamc.com/IT/zproto/pkg/metapb"
 
 	"github.com/templexxx/tsc"
-	"github.com/willf/bloom"
 )
-
-const (
-	// 8192 for delete batch, 512 for delete one by one.
-	// The batch WAL won't beyond 128KB, the normal delete WAL won't beyond 2MB. Total WAL will be < 4MB.
-	maxDirtyDelOne     = 512
-	maxDirtyDelBatch   = 8192
-	dirtyDeleteWALSize = 4 * 1024 * 1024
-	// False positive will be around 0.02.
-	maxDirtyBloomBits  = 65536
-	maxDirtyBloomHashK = 5
-)
-
-type dirtyDelete struct {
-	wal           vfs.File
-	bf            *bloom.BloomFilter
-	lastMod       int64
-	dirtyOneCnt   int
-	dirtyBatchCnt int
-}
-
-func newDirtyDelete(wal vfs.File) *dirtyDelete {
-	return &dirtyDelete{
-		wal: wal,
-		bf:  bloom.New(maxDirtyBloomBits, maxDirtyBloomHashK),
-	}
-}
-
-func (d *dirtyDelete) reset() error {
-	err := resetDirtyDelWALF(d.wal)
-	if err != nil {
-		return err
-	}
-	d.bf.ClearAll()
-	d.lastMod = 0
-	d.dirtyOneCnt = 0
-	d.dirtyBatchCnt = 0
-	return err
-}
-
-func resetDirtyDelWALF(f vfs.File) error {
-
-	err := f.Truncate(0)
-	if err != nil {
-		return err
-	}
-	return vfs.TryFAlloc(f, dirtyDeleteWALSize)
-}
 
 // updatesLoop keeps trying to get new updates request and handle it.
 // Includes:
@@ -317,77 +267,6 @@ func (e *Extenter) updatesLoop() {
 			ur.done <- orpc.ErrNotImplemented
 			continue
 		}
-	}
-}
-
-const (
-	delWALChunkSingle  = 1
-	delWALChunkBatch   = 2
-	delWALChunkMinSize = directio.BlockSize
-)
-
-// Del WAL Chunk format(https://g.tesamc.com/IT/zbuf/issues/153):
-//
-// Local struct, from low bits -> high bits.
-// type, cnt, ts, digests, padding, checksum
-func makeDelWALChunk(odigest uint32, ts int64, buf []byte) int64 {
-	buf[0] = delWALChunkSingle
-	binary.LittleEndian.PutUint32(buf[1:5], 1)
-	binary.LittleEndian.PutUint64(buf[5:13], uint64(ts))
-	binary.LittleEndian.PutUint32(buf[13:17], odigest)
-	binary.LittleEndian.PutUint32(buf[delWALChunkMinSize-4:], xdigest.Sum32(buf[:delWALChunkMinSize-4]))
-	return delWALChunkMinSize
-}
-
-func makeDelBatchWALChunk(oids []uint64, ts int64, buf []byte) int64 {
-	buf[0] = delWALChunkBatch
-	binary.LittleEndian.PutUint32(buf[1:5], uint32(len(oids)))
-	binary.LittleEndian.PutUint64(buf[5:13], uint64(ts))
-	for i, oid := range oids {
-		_, _, _, digest, _, _ := uid.ParseOID(oid)
-		binary.LittleEndian.PutUint32(buf[i*4+13:i*4+13+4], digest)
-	}
-	n := xbytes.AlignSize(13+int64(len(oids))*4+4, directio.BlockSize)
-	binary.LittleEndian.PutUint32(buf[n-4:n], xdigest.Sum32(buf[:n-4]))
-	return n
-}
-
-// readDelWALChunk reads one chunk in dirty delete WAL,
-// return:
-// isEnd(indicates reach the end or not)
-// digests(all digests in this chunk)
-// n(bytes read, chunk size too)
-func readDelWALChunk(buf []byte) (isEnd bool, ts int64, digests []uint32, n int64, err error) {
-	t := buf[0]
-	switch t {
-	case delWALChunkSingle:
-		if binary.LittleEndian.Uint32(buf[delWALChunkMinSize-4:]) != xdigest.Sum32(buf[:delWALChunkMinSize-4]) {
-			return false, 0, nil, delWALChunkMinSize,
-				xerrors.WithMessage(orpc.ErrChecksumMismatch, "failed to read dirty delete wal chunk")
-		}
-		ts = int64(binary.LittleEndian.Uint64(buf[5:13]))
-		oid := binary.LittleEndian.Uint32(buf[13:17])
-		return false, ts, []uint32{oid}, delWALChunkMinSize, nil
-	case delWALChunkBatch:
-		cnt := binary.LittleEndian.Uint32(buf[1:5])
-		if cnt == 0 {
-			return false, 0, nil, 0, xerrors.WithMessage(orpc.ErrExtentBroken,
-				"invalid dirty delete wal batch chunk with 0 oid")
-		}
-		chunkSize := xbytes.AlignSize(13+int64(cnt*4+4), directio.BlockSize)
-		if binary.LittleEndian.Uint32(buf[chunkSize-4:chunkSize]) != xdigest.Sum32(buf[:chunkSize-4]) {
-			return false, 0, nil, int64(int(chunkSize)),
-				xerrors.WithMessage(orpc.ErrChecksumMismatch, "failed to read dirty delete wal batch chunk")
-		}
-		ts = int64(binary.LittleEndian.Uint64(buf[5:13]))
-		digests = make([]uint32, cnt)
-		for i := 0; i < int(cnt); i++ {
-			digest := binary.LittleEndian.Uint32(buf[i*4+13 : i*4+13+4])
-			digests[i] = digest
-		}
-		return false, ts, digests, int64(int(chunkSize)), nil
-	default:
-		return true, 0, nil, 0, nil
 	}
 }
 
