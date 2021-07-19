d66dae1fc90298b5dd899938ed9ea9a8bb2d3527
templexxx <lucas1x1x@gmail.com>
2021-07-19 14:46:54 +0800

    extent/v1: set nil after keeper have received clone job done msg
    #293

--- extent/v1/meta.go
+++ extent/v1/meta.go
@@ -7,12 +7,22 @@ import (
 	"github.com/gogo/protobuf/proto"
 )
 
+// UpdateMeta updates meta in Extenter.
+// It's used for handling heartbeat response,
+// only ext.state & clone job (nil -> new or new -> nil) & clone job's oids_oid could be changed by heartbeat.
 func (e *Extenter) UpdateMeta(m *metapb.Extent) {
 
 	e.rwMutex.Lock()
 	if m.State != e.meta.State {
 		extutil.SetState(e.meta, m.State)
 	}
+
+	if m.CloneJob == nil {
+		if e.meta.CloneJob != nil { // Must be done.
+			e.meta.CloneJob = nil
+		}
+	}
+
 	if m.CloneJob != nil && e.meta.CloneJob == nil {
 		e.meta.CloneJob = proto.Clone(m.CloneJob).(*metapb.CloneJob)
 	}
